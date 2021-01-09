0f0fd233d92a894ce68e6b116fe73fe4b2f589a3
yogesh.ktm <yogesh.ktm@gmail.com>
2021-01-09 20:05:25 +0530

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -43,7 +43,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.JPG/" }) {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
