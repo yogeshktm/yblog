d48e45890ff110f0a2a1ebea1a2b2ea3a6204660
yogesh.ktm <yogesh.ktm@gmail.com>
2021-01-27 21:15:07 +0530

    404 links updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,12 +8,25 @@ class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>404 Not Found</h1>
+        <p>Looks like the page you are looking is not found. You may go back to the <Link to={`/`}>Homepage</Link> or read some of my latest posts below</p>
+        <ul class="suggested-article-list" style={{listStyle: 'none'}}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+                <li key={node.fields.slug}>
+                    <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    <span aria-hidden="true">🚩</span> {title}
+                    </Link>
+                </li>
+            )
+          })}
+        </ul>
       </Layout>
     )
   }
@@ -26,6 +39,20 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC },limit:4) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
       }
     }
   }
